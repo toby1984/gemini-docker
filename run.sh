f974dfc8be1c55636c1fa40106f4f8cab66429a6
Tobias Gierke <tobias.gierke@voipfuture.com>
2025-11-26 12:45:53 +0100

    improved building

--- run.sh
+++ run.sh
@@ -6,14 +6,18 @@ if [ -z "$GID" ] ; then
 fi
 export GID
 
-#
-# Build image
-#
-
-# BUILD_OPTS="--no-cache --progress=plain"
-# BUILD_OPTS="--progress=plain"
-docker compose build ${BUILD_OPTS} "$@" --build-arg UID=${UID} --build-arg USER=${USER} --build-arg GID=${GID}
+if [ "$1" == "build" ] ; then
+  #
+  # Build image
+  #
+  shift
+  BUILD_OPTS="--no-cache --progress=plain"
+  docker compose build ${BUILD_OPTS} "$@" --build-arg UID=${UID} --build-arg USER=${USER} --build-arg GID=${GID}
+  docker compose stop gemini
+  docker container rm -f gemini
+fi
 
 # start container
 docker compose create
-docker compose run --rm gemini
+# docker compose run --rm gemini
+docker compose run gemini
